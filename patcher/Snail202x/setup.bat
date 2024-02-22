797f8bfbe041425b5825ab0166fac0ba30871b24
MichaelK-F <michael@wandoo.ca>
2024-02-22 18:05:07 +0800

    re-added Snail202x as built in as opposed to a submodule modified Snail2021.c

---
+++ patcher/Snail202x/setup.bat
@@ -0,0 +1,7 @@
+@echo off
+echo This program sets up hollyhock-2 for the first time.
+
+echo Downloading Resource Hacker (not implemented yet, strange errors (blocked by site?) when trying to download directly from website using curl)
+
+echo Extracting dlls from installer using lessmsi...
+
